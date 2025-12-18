7da2947e0e4527ca26192ffc7cdd7803b4ad9443
francocasafus22 <francocasafus55@gmail.com>
2025-12-18 16:36:20 -0300

    add product entity and relation with category

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { typeOrmConfig } from './config/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { CategoriesModule } from './categories/categories.module';
       inject: [ConfigService],
     }),
     CategoriesModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Category } from '../../categories/entities/category.entity';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 40 })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    length: 120,
+    nullable: true,
+    default: 'default.svg',
+  })
+  image: string;
+
+  @Column({ type: 'decimal' })
+  price: number;
+
+  @Column({ type: 'int' })
+  inventory: number;
+
+  @ManyToOne(() => Category)
+  category: Category;
+}
