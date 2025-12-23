6c57d0c2e440823ff8d2f6c5d735fa013c2d98b0
francocasafus22 <francocasafus55@gmail.com>
2025-12-23 16:38:10 -0300

    add crud cupones

--- src/coupons/coupons.controller.ts
+++ src/coupons/coupons.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { CouponsService } from './coupons.service';
 import { CreateCouponDto } from './dto/create-coupon.dto';
 import { UpdateCouponDto } from './dto/update-coupon.dto';
@@ -22,7 +31,7 @@ export class CouponsController {
     return this.couponsService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateCouponDto: UpdateCouponDto) {
     return this.couponsService.update(+id, updateCouponDto);
   }
--- src/coupons/dto/create-coupon.dto.ts
+++ src/coupons/dto/create-coupon.dto.ts
@@ -1,5 +1,14 @@
+import { IsDateString, IsInt, IsNotEmpty, Max, Min } from 'class-validator';
+
 export class CreateCouponDto {
-  
-  
-  
+  @IsNotEmpty({ message: 'El nombre del cupón es obligatorio' })
+  name: string;
+  @IsNotEmpty({ message: 'El descuento del cupón es obligatorio' })
+  @IsInt({ message: 'El descuetno debe ser entre 1 y 100' })
+  @Max(100, { message: 'El descuento máximo es 100' })
+  @Min(1, { message: 'El descuento minimo es 1' })
+  percentage: number;
+  @IsNotEmpty({ message: 'La fecha del cupón es obligatorio' })
+  @IsDateString({}, { message: 'Fecha no válida' })
+  expirationDate: Date;
 }
