320d20759dfbcb13bc42c3caccc5efb2aed7dcfc
francocasafus22 <francocasafus55@gmail.com>
2025-12-24 19:25:48 -0300

    add category page and fetch category with products

---
+++ frontend/app/(store)/[categoryId]/page.tsx
@@ -0,0 +1,25 @@
+import { CategoryWithProductsResponseSchema } from "@/app/src/schemas";
+import ProductCard from "@/components/products/Card";
+
+type Params = Promise<{ categoryId: string }>;
+
+async function getProducts(categoryId: string) {
+  const url = `${process.env.API_URL}/categories/${categoryId}?showProducts=true`;
+  const req = await fetch(url);
+  const json = await req.json();
+  const products = CategoryWithProductsResponseSchema.parse(json);
+  return products;
+}
+
+export default async function CategoryPage({ params }: { params: Params }) {
+  const { categoryId } = await params;
+  const category = await getProducts(categoryId);
+
+  return (
+    <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl-grid-cols-3">
+      {category.products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
